e66734f93cde11012edf8056147da92d63b64048
bhimrazy <bhimrajyadav977@gmail.com>
2021-05-10 00:53:00 +0545

    chatbox setup done

--- components/Chatbox.js
+++ components/Chatbox.js
@@ -5,9 +5,14 @@ import SpeechRecognition, {
 import { useSpeechSynthesis } from "react-speech-kit";
 export default function Chatbox() {
   const [message, setMessage] = useState("");
+  const [mic, setMic] = useState(false);
   const [messages, setMessages] = useState([]);
   const [value, setValue] = useState("Hello There , How can I help You ?");
   const { speak } = useSpeechSynthesis();
+  const Greeting = {
+    message: "I am fine,Thank You . What about you , Sir ? ",
+    type: "jarvis",
+  };
   const commands = [
     // {
     //   command: ["Hello", "Hi"],
@@ -15,84 +20,82 @@ export default function Chatbox() {
     //   matchInterim: true,
     // },
     {
-      command: ["Hello"],
+      command: ["Hello", "Hi"],
       callback: ({ command }) => {
         speak({ text: value });
-        setMessage("Hello There , How can I help You ?");
+        setMessages((state) => [...state, newMessage]);
       },
       // matchInterim: true,
     },
     {
-      command: "Beijing",
-      callback: (command, spokenPhrase, similarityRatio) =>
-        setMessage(
-          `${command} and ${spokenPhrase} are ${similarityRatio * 100}% similar`
-        ),
-      // If the spokenPhrase is "Benji", the message would be "Beijing and Benji are 40% similar"
-      isFuzzyMatch: true,
-      fuzzyMatchingThreshold: 0.2,
-    },
-    {
-      command: ["eat", "sleep", "leave"],
-      callback: (command) => setMessage(`Best matching command: ${command}`),
-      isFuzzyMatch: true,
-      fuzzyMatchingThreshold: 0.2,
-      bestMatchOnly: true,
+      command: ["How"],
+      callback: ({ command }) => {
+        speak({ text: Greeting });
+        setMessages((state) => [...state, Greeting]);
+      },
+      // matchInterim: true,
     },
     {
       command: "clear",
       callback: ({ resetTranscript }) => resetTranscript(),
     },
   ];
-
-  const { transcript,resetTranscript } = useSpeechRecognition({ commands });
-  // SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
-  // if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-  //   console.log("not supported");
-  //   return null;
-  // }
-  setTimeout(() => {
-    setMessages({ jarvis: "Hello There" });
-    console.log("hello");
-  }, 3000);
-
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
+  if (mic) {
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  }
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    console.log("not supported");
+    return null;
+  }
+  const newMessage = {
+    message: "Hello There , How can I help You ?",
+    type: "jarvis",
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessages((state) => [
+      ...state,
+      {
+        message: message,
+        type: "user",
+      },
+    ]);
+    setMessage("");
+    resetTranscript();
+    // alert(`Submitting Name ${message}`);
+  };
+  const handleMic = () => {
+    setMic((v) => !v);
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      setMessages((state) => [...state, newMessage]);
+      setMic((v) => !v);
+      speak({ text: newMessage.message });
+    }, 3000);
+  }, []);
   return (
-    <div className="px-12 h-full w-full">
-      <div className="text-white">
-        <p>{message}</p>
-        <p>{transcript}</p>
-      </div>
-      <div className="relative bg-gray-50 shadow-xl rounded-2xl h-full w-full flex flex-col items-center justify-center">
-        <div className="rounded-2xl w-full px-8 py-4 flex flex-col space-y-5 overflow-y-auto scrollbar-w-2 scrollbar-track-gray-lighter scrollbar-track-gray-rounded scrollbar-thumb-rounded scrollbar-thumb-gray scrolling-touch max-h-80 text-xs text-gray-100">
-          {!messages ? (
-            <>
-              <div className="bg-indigo-400 rounded-2xl w-4/6 px-3 py-3 rounded-bl-none">
-                Hello Sir, How can I help you ?
-              </div>
-              <div className="bg-pink-400 rounded-2xl w-4/6 self-end rounded-br-none px-3 py-3 ">
-                How are You?
-              </div>
-              <div className="bg-indigo-400 rounded-2xl w-4/6 px-3 py-3 rounded-bl-none">
-                I am fine , Thankyou sir. How about You?
-              </div>
-              <div className="bg-pink-400 rounded-2xl w-4/6 self-end rounded-br-none px-3 py-3">
-                I am also fine.
-              </div>
-              <div className="bg-indigo-400 rounded-2xl w-4/6 px-3 py-3 rounded-bl-none">
-                I am fine , Thankyou sir. How about You?
-              </div>
-              <div className="bg-pink-400 rounded-2xl w-4/6 self-end rounded-br-none px-3 py-3">
-                I am also fine.
-              </div>
-              <div className="bg-indigo-400 rounded-2xl w-4/6 px-3 py-3 rounded-bl-none">
-                I am fine , Thankyou sir. How about You?
-              </div>
-              <div className="bg-pink-400 rounded-2xl w-4/6 self-end rounded-br-none px-3 py-3">
-                I am also fine.
-              </div>
-            </>
+    <div className="px-12 h-full w-full flex flex-col">
+      <div className="relative bg-gray-50 shadow-xl rounded-2xl h-full w-full flex flex-col items-center">
+        <div className="rounded-2xl w-full h-80 px-8 py-4 pb-8 flex flex-col space-y-5 overflow-y-auto scrollbar-w-2 scrollbar-track-gray-lighter scrollbar-track-gray-rounded scrollbar-thumb-rounded scrollbar-thumb-gray scrolling-touch max-h-80 text-xs text-gray-100">
+          {messages && messages.length != 0 ? (
+            messages.map((msg, index) => (
+              <>
+                <div
+                  key={index}
+                  className={` ${
+                    msg.type == "jarvis"
+                      ? `bg-indigo-400 rounded-2xl w-4/6 px-3 py-3 rounded-bl-none`
+                      : `bg-pink-400 rounded-2xl w-4/6 self-end rounded-br-none px-3 py-3 `
+                  }`}
+                >
+                  {msg.message}
+                </div>
+              </>
+            ))
           ) : (
-            <div className="justify-center items-center text-purple-600 flex">
+            <div className="justify-center items-center text-purple-600 flex py-4">
               <p className="text-2xl font-poppins font-bold">
                 Welcome To Jarvis !!!
               </p>
@@ -103,8 +106,14 @@ export default function Chatbox() {
         <div className="bg-gray-200 absolute bottom-0 rounded-b-2xl w-full py-4 px-8 flex flex-row justify-between">
           <div className="flex flex-row items-center">
             <div className="relative">
-              {/* <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-75"></span> */}
+              {mic && (
+                <span
+                  onClick={handleMic}
+                  className="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 bg-opacity-50"
+                ></span>
+              )}
               <svg
+                onClick={handleMic}
                 className="w-6 h-6 text-gray-600 border-1 border-purple-500 rounded-full cursor-pointer"
                 fill="currentColor"
                 viewBox="0 0 20 20"
@@ -121,9 +130,14 @@ export default function Chatbox() {
               Say "Hello"
             </p>
           </div>
-          <form className="flex flex-row w-5/6 justify-around space-x-3">
+          <form
+            className="flex flex-row w-5/6 justify-around space-x-3"
+            onSubmit={handleSubmit}
+          >
             <input
               type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
               className="rounded-full w-full px-2 py-1 text-xs focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-indigo-400"
             />
             <button type="submit">
@@ -139,6 +153,10 @@ export default function Chatbox() {
           </form>
         </div>
       </div>
+      {transcript && (
+        <p className="text-xs font-light text-cyan-400 py-2">
+          Transcript : {transcript}
+        </p>
+      )}
     </div>
-  );
-}
+  
